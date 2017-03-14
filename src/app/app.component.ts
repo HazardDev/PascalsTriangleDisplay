17feda8930bf2aa7450cf51c99bef84531b738b8
Logan Saso <jewsofhazard@gmail.com>
2017-03-14 01:07:14 -0700

    Code works just looks like garbage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  triangle: Array<Array<number>> = [[]];
+
+  calculateTriangle(size: number){
+    const theTriangle: Array<Array<number>> = [[1], [1, 1]];
+
+    for (let i = 2; i < size; i++) {
+      // console.log("I: " + i);
+      theTriangle.push([1]);
+      for (let x = 1; x < i; x++) {
+        // console.log(theTriangle[i][x]);
+        // console.log(theTriangle[i][x - 1]);
+        theTriangle[i].push(theTriangle[i - 1][x] + theTriangle[i - 1][x - 1]);
+      }
+      theTriangle[i].push(1);
+    }
+
+    this.triangle = theTriangle;
+
+  }
+
+
 }
